638ffaec2ea51f6a39493566cf7b16947d43c02a
Valent <valentinoleomunozch@gmail.com>
2026-07-29 12:16:42 -0500

    Fix

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "contact": "859465616359292928 (Discord ID)",
+    "id": "lang_es-valent",
+    "patches": {
+        "th105": "Scarlet Weather Rhapsody to spanish"
+    },
+    "servers": [
+        "https://mirrors.thpatch.net/Valent/"
+    ],
+    "title": "Valent's Repository"
+}
---
+++ th105/patch.js
@@ -0,0 +1,13 @@
+{
+    "id": "th105",
+    "servers": [
+        "https://mirrors.thpatch.net/Valent/th105/"
+    ],
+    "title": "Scarlet Weather Rhapsody to Spanish",
+    "dependencies": [
+        "nmlgc/base_tsa",
+        "nmlgc/base_tasofro",
+        "nmlgc/western_name_order"
+        "nmlgc/script_latin"
+    ]
+}
